2985d7b09136583c1ba68d4623f17982cc2be5dc
Max <mujd19@gmail.com>
2021-03-24 22:16:07 -0300

    ..

--- models/server.js
+++ models/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
+
+    // Conectar a base de datos
+    this.conectarDB();
+
     // Middlewares
     this.middlewares();
 
@@ -14,6 +19,10 @@ class Server {
 
     // Rutas de mi app
     this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
